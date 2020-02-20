2d903116e37a32fd8ae9b4a5442128a7c8d59f86
Caroline Fassot <fassot.caroline@gmaiL.com>
2020-02-20 11:19:57 +0100

    index.php et .css

---
+++ index.php
@@ -0,0 +1,10 @@
+<header style="background-color:black;">
+  <div class="">
+    <img src="asset/img/header/enah_logo.png" alt="École Nationale des Activités Hippiques Logo"/>
+    <img src="asset/img/usa-today.svg" alt="note 5" class="note"/>
+  </div>
+  <div class="">
+    <img src="asset/img/header/user.svg" alt="compte"/>
+    <img src="asset/img/header/dowload.svg" alt="télécharger la documentation"/>
+  </div>
+</header
