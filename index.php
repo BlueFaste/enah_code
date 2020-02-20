d742542c524ba6edf3e438acf5c4237d5a3bd0c5
Caroline Fassot <fassot.caroline@gmaiL.com>
2020-02-20 16:48:19 +0100

    code de la page d'accueil

--- index.php
+++ index.php
@@ -1,10 +1,96 @@
-<header style="background-color:black;">
-  <div class="">
-    <img src="asset/img/header/enah_logo.png" alt="École Nationale des Activités Hippiques Logo"/>
-    <img src="asset/img/usa-today.svg" alt="note 5" class="note"/>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="L'École Nationale des Activités Hippiques Le Centre de formation propose un programme spécialisé dans les activités hippiques visant le perfectionnement du cavalier.
+    L’objectif étant qu’ils deviennent des professionnels : « Homme de Cheval », avec des connaissances leur permettant d’accéder à des postes à responsabilités dans les entreprises ou prétendre à leur propre installation.">
+    <meta name="viewport" content="width=device-width, initial-scalable=1">
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="index.css">
+    <title>Enah-code</title>
+  </head>
+  <body>
+
+    <div class="head_accueil" style="background-image: url(asset/img/accueil/horse-3609347_1920.jpg);">
+    <header>
+      <div class="">
+        <img src="asset/img/header/enah_logo.png" alt="École Nationale des Activités Hippiques Logo" class="logo"/>
+      </div>
+      <div class="compte_telechargement">
+        <img src="asset/img/header/user.svg" alt="compte" class="header_icone"/>
+        <img src="asset/img/header/download.svg" alt="télécharger la documentation" class="header_icone"/>
+      </div>
+    </header>
+
+      <p> <span>E</span>cole <span>N</span>ationale des <span>A</span>ctivités <span>H</span>ippiques </p>
+
+      <div class="">
+        <a href="#test"><img src="asset/img/accueil/next.svg" alt="descendre la page" class="fleche"/></a>
+        <img src="asset/img/accueil/france.svg" alt="Changer de langue en Français" class="langues"/>
+        <img src="asset/img/accueil/usa-today.svg" alt="Changer de langue en Anglais" class="langues"/>
+        <img src="asset/img/accueil/spain.svg" alt="Changer de langue en Espagnol" class="langues"/>
+      </div>
+
+    </div>
+
+    <nav role="navigation" id="test">
+				  <div id="menuToggle">
+
+					<input type="checkbox" />
+
+					  <span></span>
+					  <span></span>
+					  <span></span>
+
+					<ul id="menu">
+						<a href="#"><li>Notre univers</li></a>
+						<a href="#"><li>Formation</li></a>
+						<a href="#"><li>Actualités et Êvénement</li></a>
+						<a href="#"><li>Nos partenaires</li></a>
+						<a href="#"><li>Infos & Services</li></a>
+
+					</ul>
+				  </div>
+				</nav>
+
+<h1 class="accueil">
+  Accueil
+</h1>
+
+<div class="projet">
+
+  <div style="background-image:url(asset/img/accueil/horse-1144061_1920.jpg)" class="img_projet">
   </div>
-  <div class="">
-    <img src="asset/img/header/user.svg" alt="compte"/>
-    <img src="asset/img/header/dowload.svg" alt="télécharger la documentation"/>
+  <div class="titre_projet">
+    <h2>
+      Notre projet
+    </h2>
+    <p class="texte_projet">Le Centre de formation propose un programme spécialisé dans les activités hippiques visant le perfectionnement du cavalier.</p>
+
+    <p class="texte_projet">L’objectif étant qu’ils deviennent des professionnels : « Homme de Cheval », avec des connaissances leur permettant d’accéder à des postes à responsabilités dans les entreprises ou prétendre à leur propre installation.</p>
+
   </div>
-</header
+
+  <img src="" alt="télécharger la documentation"/>
+
+</div>
+
+<div>
+  <div class="univers" style="background-image: url(asset/img/accueil/)">
+    <p>Notre Univers</p>
+  </div>
+  <div class="formation" style="background-image: url(asset/img/accueil/)">
+    <p>Notre Formations</p>
+  </div>
+  <div class="valeur" style="background-image: url(asset/img/accueil/)">
+    <p>Nos Valeurs</p>
+  </div>
+</div>
+
+
+</div>
+
+
+
+  </body>
+</html>
